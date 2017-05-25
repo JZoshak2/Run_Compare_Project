719b45e06b5c84951c977e9033a82d6284136b0b
John Zoshak <JohnZoshak@Johns-MacBook-Air-2.local>
2017-05-24 22:33:07 -0400

    ORM relationships up, beignnings of API routes

--- models/users.js
+++ models/users.js
@@ -1,9 +1,40 @@
 module.exports = function(sequelize, DataTypes) {
-  var Users = sequelize.define("Users", {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    zip: DataTypes.INTEGER
-  });
+  var Users = sequelize.define("user", {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    firstName: {
+      type: Sequelize.STRING,
+      notEmpty: true
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      notEmpty: true
+    },
+    email: {
+      type: Sequelize.STRING,
+      notEmpty: true
+    },
+    password: {
+      type: Sequelize.STRING,
+      notEmpty: true
+    },
+    zip: {
+      type: Sequelize.INTEGER,
+      notEmpty: true
+    }
+  },
+    {
+      classMethods: {
+        associate: function(models) {
+          Users.hasMany(models.Runs, {
+            onDelete: "cascade",
+          });
+        }
+      }
+    }
+  );
   return Users;
 };
