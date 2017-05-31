d7a9072f98c16db844d7fc86dfbec68b6e5ee195
cstuckey <chrislstuckey@gmail.com>
2017-05-31 19:11:12 -0400

    modified server to work with routes

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-var htmlRoutes = require("./routes/html-routes.js");
+var db = require("./models");
+// var htmlRoutes = require("./routes/html-routes.js");
 
 //Passport Setup
 // var passport = require("passport");
@@ -32,13 +33,31 @@ var htmlRoutes = require("./routes/html-routes.js");
 var app = express();
 var PORT = 3000;
 
+// Serve static content for the app from the "public" directory in the application directory.
+// app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.static(process.cwd() + "/public"))
+
 //Body parser Stuff
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+// Dependency
+var exphbs = require("express-handlebars");
+// Set handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// Import routes and give server access to them.
+var db = require("./models");
+var routes = require("./controllers/home.js");
+var api = require("./routes/api-routes.js");
+
+// Routes
+app.use("/", routes);
+api.apiRoutes(app);
+
 //User Login
 // app.post('/login',
 //   passport.authenticate('local', { successRedirect: '/',
@@ -47,9 +66,12 @@ app.use('/', express.static(path.join(__dirname, 'public')))
 // );
 
 //routes
-htmlRoutes(app);
+// htmlRoutes(app);
 
-//ServerListening
-app.listen(PORT, function(){
-  console.log("Listening on PORT " + PORT);
+
+//Sync sequelize models and Start Express App. Server Listening
+db.sequelize.sync( {force: true} ).then(function() {
+	app.listen(PORT, function(){
+	  console.log("Listening on PORT " + PORT);
+	});  
 });
