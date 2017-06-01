1466fa7e06c56c965995fbc428dfe2b350b18228
cstuckey <chrislstuckey@gmail.com>
2017-05-31 21:17:01 -0400

    api routes shows data on rankings page

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,31 +1,35 @@
 //Write API routes.
+var express = require("express");
 
+var router = express.Router();
 
 var db = require("../models");
 
-module.exports.apiRoutes = function(app) {
 
   // POST route for saving a new todo
-  app.post("/api/runs", function(req, res) {
+  router.post("/api/runs", function(req, res) {
     console.log(req.body);
     // create takes an argument of an object describing the item we want to
     // insert into our table. In this case we just we pass in an object with a text
     // and complete property (req.body)
+
+    //need to write function to calc pace
     db.Runs.create({
       distance: req.body.distance,
       time: req.body.time,
-      pace: req.body.pace, //need to write function to calc pace
-      run_notes: req.body.note
+      pace: req.body.pace, 
+      run_notes: req.body.run_notes
 
     }).then(function(dbTodo) {
       // We have access to the new todo as an argument inside of the callback function
-      res.json(dbTodo);
+      res.redirect("/rankings");
     });
   });
 
-  // app.post("/api/runresults", function(req, res){
+  // router.post("/api/runresults", function(req, res){
   //   db.Users.create({
       
   //   });
   // });
-};
+
+module.exports = router;
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,23 +1,46 @@
 // HTML ROUTES
+var express = require("express");
+var db = require("../models");
 var path = require("path");
-
-module.exports = function(app) {
+var router = express.Router();
 
 // app.get("/", function(req, res){
 //   res.sendFile(path.join(__dirname, "../public/"));
 // });
 
-app.get("/login", function(req, res){
+router.get("/", function(req, res) {
+  res.render("home");
+});
+// Route to registration (register) page
+router.get("/signup", function(req, res) {
+  res.render("signup");
+});
+// Route to Sign In (login) page
+router.get("/login", function(req, res){
   res.render("login");
 });
 
-app.get("/register", function(req, res){
-  res.render("register");
+router.get("/logRun", function(req, res) {
+  res.render("logRun");
 });
 
-app.get("/rankings", function(req, res){
-  res.render("rankings", {
-  	runs: data
-  });
+router.get("/logOrRank", function(req, res) {
+  res.render("logOrRank");
 });
-};
+
+router.get("/rankings", function(req, res){
+  // db.Runs.findAll({
+  // 	where: {
+  // 		usersId: user
+  // 	}
+  // })
+  db.Runs.findAll({}).then(function(data){
+  	console.log(data);
+  	res.render("rankings", { 
+  		results: data
+  	});
+  });
+  
+});
+
+module.exports = router;
